29a1c784d5fa5fc7380ca73cba9892f8b1ac30fc
cendon.thamyle@gmail.com <thamyle_cendon@192.168.0.7>
2026-07-20 17:18:27 -0300

    docs: adiciona documentação Javadoc ao enum TipoImovel

--- src/br/com/flexolx/model/enums/TipoImovel.java
+++ src/br/com/flexolx/model/enums/TipoImovel.java
@@ -1,5 +1,12 @@
 package br.com.flexolx.model.enums;
 
+/**
+ * Enumeração dos tipos de imóveis suportados pelo catálogo do sistema.
+ * * <p>Esta padronização é fundamental para guiar os filtros de busca, pesquisas por categoria
+ * e a correta instanciação/classificação das propriedades cadastradas.</p>
+ * * @author Thamyle Cendon
+ * @version 1.0
+ */
 public enum TipoImovel {
     CASA,
     APARTAMENTO,
