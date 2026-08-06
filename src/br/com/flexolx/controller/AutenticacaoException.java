0213db61a68a8c1fe9050792f17d8ab7d1f90f9d
lucaphill <llucaphill@gmail.com>
2026-08-05 21:39:48 -0300

    documentação atualizada para excessões

--- src/br/com/flexolx/controller/AutenticacaoException.java
+++ src/br/com/flexolx/controller/AutenticacaoException.java
@@ -1,5 +1,12 @@
 package br.com.flexolx.controller;
 
+/**
+ * Exceção lançada quando uma tentativa de login falha por e-mail
+ * ou senha incorretos.
+ *
+ * @author Luca Borges
+ * @version 1.0
+ */
 public class AutenticacaoException extends RuntimeException{
 
     public AutenticacaoException(String mensagem){
--- src/br/com/flexolx/controller/PersistenciaException.java
+++ src/br/com/flexolx/controller/PersistenciaException.java
@@ -1,5 +1,12 @@
 package br.com.flexolx.controller;
 
+/**
+ * Exceção lançada quando ocorre um erro durante o salvamento ou
+ * carregamento de dados persistidos em arquivo.
+ *
+ * @author Luca Borges
+ * @version 1.0
+ */
 public class PersistenciaException extends RuntimeException {
 
     public PersistenciaException(String mensagem, Throwable causa){
